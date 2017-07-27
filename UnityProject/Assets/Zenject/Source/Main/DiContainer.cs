e9c4a4f1c4d43d165761834cc8ed17ebf534850a
marsermd <mischapanin@gmail.com>
2017-07-27 16:06:53 +0300

    ZenjectAllowDuringValidation now works on interfaces #284

--- UnityProject/Assets/Zenject/Source/Main/DiContainer.cs
+++ UnityProject/Assets/Zenject/Source/Main/DiContainer.cs
@@ -735,7 +735,7 @@ namespace Zenject
                 || type.DerivesFrom<Context>()
 #endif
 #if !(UNITY_WSA && ENABLE_DOTNET && !UNITY_EDITOR)
-                || type.HasAttribute<ZenjectAllowDuringValidationAttribute>()
+                || type.HasAttributeIncludingInterfaces<ZenjectAllowDuringValidationAttribute>()
 #endif
             ;
         }
