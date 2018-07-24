56e6e9c5095575c89e2dc9f38851a5089ab5d0fb
Steve Vermeulen <sfvermeulen@gmail.com>
2018-07-24 17:13:32 +0800

    Fix issue with struct type signals on AOT platforms (issue #510)

--- UnityProject/Assets/Plugins/Zenject/OptionalExtras/SampleGame2 (Advanced)/Scripts/Installers/GameSignalsInstaller.cs
+++ UnityProject/Assets/Plugins/Zenject/OptionalExtras/SampleGame2 (Advanced)/Scripts/Installers/GameSignalsInstaller.cs
@@ -3,6 +3,15 @@ using Zenject;
 
 namespace Zenject.SpaceFighter
 {
+    // Include this just to ensure BindSignal with an object mapping works
+    public class PlayerDiedSignalObserver
+    {
+        public void OnPlayerDied()
+        {
+            Debug.Log("Fired PlayerDiedSignal");
+        }
+    }
+
     public class GameSignalsInstaller : Installer<GameSignalsInstaller>
     {
         public override void InstallBindings()
@@ -13,7 +22,7 @@ namespace Zenject.SpaceFighter
             Container.DeclareSignal<PlayerDiedSignal>();
 
             // Include these just to ensure BindSignal works
-            Container.BindSignal<PlayerDiedSignal>().ToMethod(() => Debug.Log("Fired PlayerDiedSignal"));
+            Container.BindSignal<PlayerDiedSignal>().ToMethod<PlayerDiedSignalObserver>(x => x.OnPlayerDied).FromNew();
             Container.BindSignal<EnemyKilledSignal>().ToMethod(() => Debug.Log("Fired EnemyKilledSignal"));
         }
     }
